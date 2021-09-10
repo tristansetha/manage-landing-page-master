b08fab184540adac019699e197fed29198e5002d
ts <tristansetha@gmail.com>
2021-09-09 19:29:43 -0700

    testimonial slider mobile view

--- components/slider/slider.js
+++ components/slider/slider.js
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import styles from './slider.module.css'
 import { useState, useEffect } from 'react'
+import useMediaQuery from '../../hooks/useMediaQuery'
 
-export const Slide = ({ src, title, testimonial }) => {
+const Slide = ({ src, title, testimonial }) => {
   return (
     <>
       <div className={styles.testimonialCard}>
@@ -24,75 +25,143 @@ export const Slide = ({ src, title, testimonial }) => {
 }
 
 export default function Slider({ testimonials }) {
+
+  const isBreakpoint = useMediaQuery(1440)
   let tempTestimonials = testimonials
+
+
+  const [slideIndex, setSlideIndex] = useState({
+    pixels: 0,
+    dot: 0,
+    transition: 1
+  })
+
   const [translateX, setTranslateX] = useState({
     pixels: 0,
     count: 0,
     transition: 1
   })
-
-  const handleClassChange = () => {
-    if (translateX.count === tempTestimonials.length - 3) {
-      setTranslateX({
-        transition: 0,
-        pixels: 0,
-        count: 0
-      })
-    } else {
-      setTranslateX({
-        transition: 1,
-        count: translateX.count + 1,
-        pixels: translateX.pixels + 570
-      })
+  // DESKTOP
+  if (!isBreakpoint) {
+    const handleTranslate = () => {
+      if (translateX.count === tempTestimonials.length - 3) {
+        setTranslateX({
+          transition: 0,
+          pixels: 0,
+          count: 0
+        })
+      } else {
+        setTranslateX({
+          transition: 1,
+          count: translateX.count + 1,
+          pixels: translateX.pixels + 570
+        })
+      }
     }
+
+    setTimeout(() => {
+      console.log("trigger")
+      handleTranslate()
+    }, 6000)
+    return (
+      <>
+        <div className={styles.testimonialContainer}>
+          <div className={styles.testimonialTitle}>
+            What they’ve said
+          </div>
+          <div
+            style={{
+              transform: `translateX(${translateX.pixels}px)`, transition: `all ${translateX.transition}s`
+            }}
+            className={styles.testimonialSlider}>
+            {tempTestimonials.map((element, index) => (
+              <Slide
+                key={index}
+                src={element.src}
+                title={element.title}
+                testimonial={element.testimonial}
+              />
+            ))}
+          </div>
+          <div className={styles.testimonialNavigation}>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+          </div>
+          <div className={styles.testimonialButtonContainer}>
+            <button>Get Started</button>
+          </div>
+        </div>
+      </>
+    );
+    // MOBILE
+  } else {
+    const handleSlideIndex = index => (e) => {
+      console.log(index)
+      if (index === 0) {
+        setSlideIndex({
+          ...slideIndex,
+          dot: index,
+          pixels: 0
+        })
+      } else if (index === 1) {
+        setSlideIndex({
+          ...slideIndex,
+          dot: index,
+          pixels: -370
+        })
+      } else if (index === 2) {
+        setSlideIndex({
+          ...slideIndex,
+          dot: index,
+          pixels: -740
+        })
+      } else if (index === 3) {
+        setSlideIndex({
+          ...slideIndex,
+          dot: index,
+          pixels: -1110
+        })
+      }
+    }
+
+    return (
+      <>
+        <div className={styles.testimonialContainer}>
+          <div className={styles.testimonialTitle}>
+            What they’ve said
+          </div>
+          <div
+            style={{ transform: `translateX(${slideIndex.pixels}px)`, transition: `all 1s` }}
+            className={styles.testimonialSlider}>
+            {tempTestimonials.map((element, index) => (
+              <Slide
+                key={index}
+                src={element.src}
+                title={element.title}
+                testimonial={element.testimonial}
+              />
+            ))}
+          </div>
+          <div className={styles.testimonialNavigation}>
+            <span onClick={handleSlideIndex(0)} className={styles.dot}></span>
+            <span onClick={handleSlideIndex(1)} className={styles.dot}></span>
+            <span onClick={handleSlideIndex(2)} className={styles.dot}></span>
+            <span onClick={handleSlideIndex(3)} className={styles.dot}></span>
+          </div>
+          <div className={styles.testimonialButtonContainer}>
+            <button>Get Started</button>
+          </div>
+        </div>
+      </>
+    );
+
+
   }
 
-  setTimeout(() => {
-    handleClassChange()
-    console.log(translateX)
-  }, 2000)
 
-  // useEffect(() => {
-  //   if (translateX.count === tempTestimonials.length - 2) {
-  //     setTranslateX({
-  //       transition: 0,
-  //       pixels: 0,
-  //       count: 0
-  //     })
-  //   }
-  // }), [translateX]
 
-  return (
-    <>
-      <div className={styles.testimonialContainer}>
-        <div className={styles.testimonialTitle}>
-          What they’ve said
-        </div>
-        <div
-          style={{
-            transform: `translateX(${translateX.pixels}px)`, transition: `all ${translateX.transition}s`
-          }}
-          className={styles.testimonialSlider}>
-          {tempTestimonials.map((element, index) => (
-            <Slide
-              key={index}
-              src={element.src}
-              title={element.title}
-              testimonial={element.testimonial}
-            />
-          ))}
-        </div>
-        <div className={styles.testimonialNavigation}>
-          <span className={styles.dot}></span>
-          <span className={styles.dot}></span>
-          <span className={styles.dot}></span>
-          <span className={styles.dot}></span>
-        </div>
-        <div className={styles.testimonialButtonContainer}>
-          <button>Get Started</button>
-        </div>
-      </div>
-    </>
-  );
+
 }
 
--- pages/testimonials.js
+++ pages/testimonials.js
@@ -1,37 +1,37 @@
 export const testimonialData = [
   {
     src: "/avatar-richard.png",
-    title: "Richard Watts",
+    title: "0 Richard Watts",
     testimonial: "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!"
   },
   {
     src: "/avatar-shanai.png",
-    title: "Shanai Gough",
+    title: "1 Shanai Gough",
     testimonial: "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive."
   },
   {
     src: "/avatar-ali.png",
-    title: "Ali Bravo",
+    title: "2 Ali Bravo",
     testimonial: "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."
   },
   {
     src: "/avatar-anisha.png",
-    title: "Anisha Li",
+    title: "3 Anisha Li",
     testimonial: "Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."
   },
   {
     src: "/avatar-richard.png",
-    title: "Richard Watts",
+    title: "4 Richard Watts",
     testimonial: "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!"
   },
   {
     src: "/avatar-shanai.png",
-    title: "Shanai Gough",
+    title: "5 Shanai Gough",
     testimonial: "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive."
   },
   {
     src: "/avatar-ali.png",
-    title: "Ali Bravo",
+    title: "6 Ali Bravo",
     testimonial: "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."
   },
 ]
